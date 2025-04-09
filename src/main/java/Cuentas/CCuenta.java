228f1d53a6a47b2851c4d8d064380dbe2ca36977
Jose Carlos <jose.gonzalezh@protonmail.com>
2025-04-09 19:05:51 +0100

    Mejora del JavaDoc autogenerado.

--- src/main/java/Cuentas/CCuenta.java
+++ src/main/java/Cuentas/CCuenta.java
@@ -1,9 +1,15 @@
 package Cuentas;
 
 /**
+ * La clase CCuenta representa una cuenta bancaria que permite realizar
+ * operaciones básicas como ingresar dinero, retirar dinero y consultar el
+ * saldo. También incluye atributos relacionados con el titular y el tipo de
+ * interés.
+ *
+ * Esta clase está pensada para demostrar encapsulamiento y uso de métodos con
+ * control de errores mediante excepciones.
  *
  * @author jose
- * 
  */
 public class CCuenta {
 
@@ -13,11 +19,12 @@ public class CCuenta {
     private double tipoInterés;
 
     /**
+     * Constructor de la clase CCuenta.
      *
-     * @param nom
-     * @param cue
-     * @param sal
-     * @param tipo
+     * @param nom el nombre del titular de la cuenta
+     * @param cue el numero o identificador de la cuenta
+     * @param sal el saldo inicial
+     * @param tipo el tipo de interes
      */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
@@ -26,13 +33,17 @@ public class CCuenta {
     }
 
     /**
-     * @return the nombre
+     * Obtiene el nombre del titular de la cuenta
+     *
+     * @return el nombre del titular
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
+     * Pone el nombre del titular de la cuenta
+     *
      * @param nombre the nombre to set
      */
     public void setNombre(String nombre) {
@@ -40,13 +51,17 @@ public class CCuenta {
     }
 
     /**
-     * @return the cuenta
+     * Obtiene el número o identificador de la cuenta.
+     *
+     * @return el num de cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
+     * Establece el número o identificador de la cuenta.
+     *
      * @param cuenta the cuenta to set
      */
     public void setCuenta(String cuenta) {
@@ -54,7 +69,9 @@ public class CCuenta {
     }
 
     /**
-     * @return the saldo
+     * Obtiene el saldo actual de la cuenta.
+     *
+     * @return el saldo disponible
      */
     public double getSaldo() {
         return saldo;
@@ -68,13 +85,17 @@ public class CCuenta {
     }
 
     /**
-     * @return the tipoInterés
+     * Obtiene el tipo de interés asociado a la cuenta.
+     *
+     * @return el tipoInterés
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
+     * Establece el tipo de interés de la cuenta.
+     *
      * @param tipoInterés the tipoInterés to set
      */
     public void setTipoInterés(double tipoInterés) {
@@ -82,17 +103,19 @@ public class CCuenta {
     }
 
     /**
+     * Devuelve el saldo actual de la cuenta.
      *
-     * @return
+     * @return saldo disponible
      */
     public double estado() {
         return getSaldo();
     }
 
     /**
+     * Ingresa una cantidad positiva en la cuenta.
      *
-     * @param cantidad
-     * @throws Exception
+     * @param cantidad a ingresar
+     * @throws Exception si la cantidad es negativa
      */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0) {
@@ -102,9 +125,10 @@ public class CCuenta {
     }
 
     /**
+     * Retira una cantidad del saldo de la cuenta, si hay suficiente dinero.
      *
-     * @param cantidad
-     * @throws Exception
+     * @param cantidad a retirar
+     * @throws Exception si la cantidad es negativa o insuficiente saldo.
      */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0) {
